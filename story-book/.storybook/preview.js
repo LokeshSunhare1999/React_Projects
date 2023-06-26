ec95746fd5cbc0b54a6f1ecf5af1f89349c0c48b
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2023-06-27 00:12:05 +0530

    storybook

--- story-book/.storybook/preview.js
+++ story-book/.storybook/preview.js
@@ -8,6 +8,10 @@ const preview = {
         date: /Date$/,
       },
     },
+    options: {
+      storySort: (a, b) =>
+        a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
+    },
   },
 };
 
