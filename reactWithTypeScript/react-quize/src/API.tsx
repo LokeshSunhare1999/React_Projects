494747b5f5803e01541f2bf6b9519799d5d7e713
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2023-10-28 18:58:32 +0530

    react with TS

---
+++ reactWithTypeScript/react-quize/src/API.tsx
@@ -0,0 +1,31 @@
+import { shuffleArray } from "./utils";
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answer: string[] };
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answer,
+      question.correct_answer,
+    ]),
+  }));
+};
